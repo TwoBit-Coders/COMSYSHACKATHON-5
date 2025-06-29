c30ab57ba1db37a997a59d3916e6ef2eb5ce8dae
ArchieCodespiece <archismancodev@gmail.com>
2025-06-29 23:05:19 +0530

    Added gender prediction model

--- modelcreation.py
+++ modelcreation.py
@@ -14,8 +14,8 @@ print(f"Using device: {device}")
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406],  # Mean for ImageNet
-                         [0.229, 0.224, 0.225])  # Std for ImageNet
+    transforms.Normalize([0.485, 0.456, 0.406],  t
+                         [0.229, 0.224, 0.225])  
 ])
 
 # STEP 3: Loading the  Dataset
